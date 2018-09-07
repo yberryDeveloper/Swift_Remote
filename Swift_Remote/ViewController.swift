f31e2cfb3abeb9fd3dccc2c66f06f5ab662c71f1
puneet <puneet.yberry@gmail.com>
2018-09-07 11:30:35 +0530

    Initial Commit

---
+++ Swift_Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Remote
+//
+//  Created by mac on 07/09/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
