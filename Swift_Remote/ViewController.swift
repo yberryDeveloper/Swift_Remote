888bee6f886ec4836a4c1da44812c796ce4eea6f
puneet <puneet.yberry@gmail.com>
2018-09-07 11:53:16 +0530

    Hello Maratha

--- Swift_Remote/ViewController.swift
+++ Swift_Remote/ViewController.swift
@@ -12,17 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-print("Hello Puneet")
-        
-        
-        
+        print("Hello Puneet")
+        print("Hello Maratha")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
